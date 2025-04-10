419d13206a9571f1e1981633a6af727824979053
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-04-10 15:50:57 +0200

    adding animation to splash view

--- lib/features/splash/presention/views/widgets/splash_view_body.dart
+++ lib/features/splash/presention/views/widgets/splash_view_body.dart
@@ -1,8 +1,31 @@
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Offset> slidingAnimation;
+  late Animation<double> fadeAnimation;
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 260));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, -2), end: Offset.zero)
+            .animate(_animationController);
+    fadeAnimation =
+        Tween<double>(begin: 0, end: 1).animate(_animationController);
+    _animationController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -10,11 +33,28 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
-        const Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-        ),
+        const SizedBox(height: 4),
+        AnimatedBuilder(
+            animation: slidingAnimation,
+            builder: (context, _) {
+              return FadeTransition(
+                opacity: fadeAnimation,
+                child: SlideTransition(
+                  position: slidingAnimation,
+                  child: const Text(
+                    'Read Free Books',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              );
+            }),
       ],
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
   }
 }
