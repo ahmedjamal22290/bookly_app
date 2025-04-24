a6cb38ea76b1843acffdbac84988fad803ccd083
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-04-24 13:08:13 +0200

    add appBar to search view

---
+++ lib/features/search/presentation/views/widgets/search_view_appbar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SearchViewAppBar extends StatelessWidget {
+  const SearchViewAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 25),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          IconButton(
+            onPressed: () {
+              return context.pop();
+            },
+            icon: const Icon(
+              Icons.arrow_back,
+              size: 20,
+            ),
+          ),
+          const Spacer(),
+          Image.asset(
+            'assets/images/Logo.png',
+            height: 35,
+            width: 115,
+          ),
+          const Spacer()
+        ],
+      ),
+    );
+  }
+}
--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,12 +1,19 @@
+import 'package:bookly_app/core/utils/app_router.dart';
+import 'package:bookly_app/features/search/presentation/views/widgets/search_view_appBar.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(),
+    return const SafeArea(
+      child: Column(
+        children: [
+          SearchViewAppBar(),
+        ],
+      ),
     );
   }
 }
