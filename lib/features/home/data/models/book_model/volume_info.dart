2594d8151edbd7155d3c80e9e6336c3403f6eef0
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-04-28 18:39:35 +0300

    refactor model

--- lib/features/home/data/models/book_model/volume_info.dart
+++ lib/features/home/data/models/book_model/volume_info.dart
@@ -1,26 +1,22 @@
 import 'package:equatable/equatable.dart';
 
-import 'image_links.dart';
 import 'industry_identifier.dart';
 import 'panelization_summary.dart';
 import 'reading_modes.dart';
 
 class VolumeInfo extends Equatable {
   final String? title;
+  final String? subtitle;
   final List<String>? authors;
-  final String? publisher;
   final String? publishedDate;
   final List<IndustryIdentifier>? industryIdentifiers;
   final ReadingModes? readingModes;
-  final int? pageCount;
   final String? printType;
-  final int? averageRating;
-  final int? ratingsCount;
+  final List<String>? categories;
   final String? maturityRating;
   final bool? allowAnonLogging;
   final String? contentVersion;
   final PanelizationSummary? panelizationSummary;
-  final ImageLinks? imageLinks;
   final String? language;
   final String? previewLink;
   final String? infoLink;
@@ -28,20 +24,17 @@ class VolumeInfo extends Equatable {
 
   const VolumeInfo({
     this.title,
+    this.subtitle,
     this.authors,
-    this.publisher,
     this.publishedDate,
     this.industryIdentifiers,
     this.readingModes,
-    this.pageCount,
     this.printType,
-    this.averageRating,
-    this.ratingsCount,
+    this.categories,
     this.maturityRating,
     this.allowAnonLogging,
     this.contentVersion,
     this.panelizationSummary,
-    this.imageLinks,
     this.language,
     this.previewLink,
     this.infoLink,
@@ -50,8 +43,10 @@ class VolumeInfo extends Equatable {
 
   factory VolumeInfo.fromJson(Map<String, dynamic> json) => VolumeInfo(
         title: json['title'] as String?,
-        authors: json['authors'] as List<String>?,
-        publisher: json['publisher'] as String?,
+        subtitle: json['subtitle'] as String?,
+        authors: (json['authors'] as List<dynamic>?)
+            ?.map((e) => e as String)
+            .toList(),
         publishedDate: json['publishedDate'] as String?,
         industryIdentifiers: (json['industryIdentifiers'] as List<dynamic>?)
             ?.map((e) => IndustryIdentifier.fromJson(e as Map<String, dynamic>))
@@ -60,10 +55,10 @@ class VolumeInfo extends Equatable {
             ? null
             : ReadingModes.fromJson(
                 json['readingModes'] as Map<String, dynamic>),
-        pageCount: json['pageCount'] as int?,
         printType: json['printType'] as String?,
-        averageRating: json['averageRating'] as int?,
-        ratingsCount: json['ratingsCount'] as int?,
+        categories: (json['categories'] as List<dynamic>?)
+            ?.map((e) => e as String)
+            .toList(),
         maturityRating: json['maturityRating'] as String?,
         allowAnonLogging: json['allowAnonLogging'] as bool?,
         contentVersion: json['contentVersion'] as String?,
@@ -71,9 +66,6 @@ class VolumeInfo extends Equatable {
             ? null
             : PanelizationSummary.fromJson(
                 json['panelizationSummary'] as Map<String, dynamic>),
-        imageLinks: json['imageLinks'] == null
-            ? null
-            : ImageLinks.fromJson(json['imageLinks'] as Map<String, dynamic>),
         language: json['language'] as String?,
         previewLink: json['previewLink'] as String?,
         infoLink: json['infoLink'] as String?,
@@ -82,21 +74,18 @@ class VolumeInfo extends Equatable {
 
   Map<String, dynamic> toJson() => {
         'title': title,
+        'subtitle': subtitle,
         'authors': authors,
-        'publisher': publisher,
         'publishedDate': publishedDate,
         'industryIdentifiers':
             industryIdentifiers?.map((e) => e.toJson()).toList(),
         'readingModes': readingModes?.toJson(),
-        'pageCount': pageCount,
         'printType': printType,
-        'averageRating': averageRating,
-        'ratingsCount': ratingsCount,
+        'categories': categories,
         'maturityRating': maturityRating,
         'allowAnonLogging': allowAnonLogging,
         'contentVersion': contentVersion,
         'panelizationSummary': panelizationSummary?.toJson(),
-        'imageLinks': imageLinks?.toJson(),
         'language': language,
         'previewLink': previewLink,
         'infoLink': infoLink,
@@ -107,20 +96,17 @@ class VolumeInfo extends Equatable {
   List<Object?> get props {
     return [
       title,
+      subtitle,
       authors,
-      publisher,
       publishedDate,
       industryIdentifiers,
       readingModes,
-      pageCount,
       printType,
-      averageRating,
-      ratingsCount,
+      categories,
       maturityRating,
       allowAnonLogging,
       contentVersion,
       panelizationSummary,
-      imageLinks,
       language,
       previewLink,
       infoLink,
