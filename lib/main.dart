5d3e6dc6cad1a34161209891963a71b195653ba7
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-04-29 15:00:41 +0300

    setup getIt in main

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  // HomeRepoImpl(apiService: ApiService(Dio())).fetchNewestBooks();
+  setupGetIt();
   runApp(const BooklyApp());
 }
 
