500dfdfcf4e7608a437e492d65ea35d5a71114c9
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-04-30 16:52:24 +0300

    use cubit in best seller list

--- lib/features/home/presentation/views/widgets/best_seller_list_view.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view.dart
@@ -1,18 +1,35 @@
+import 'package:bookly_app/core/widgets/custom_error_text.dart';
+import 'package:bookly_app/core/widgets/custom_loading_indicator.dart';
+import 'package:bookly_app/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_list_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BestSellerListView extends StatelessWidget {
   const BestSellerListView({super.key});
-  final length = 9;
+
   @override
   Widget build(BuildContext context) {
-    return SliverList(
-      delegate: SliverChildBuilderDelegate((context, index) {
-        if (index == length - 1) {
-          return SizedBox(height: MediaQuery.of(context).size.height * 0.0728);
+    return BlocBuilder<NewestBooksCubit, NewestBooksState>(
+      builder: (context, state) {
+        if (state is NewestBooksSuccessState) {
+          return SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+              if (index == state.books.length - 1) {
+                return SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.0728);
+              }
+              return const BookListItem();
+            }, childCount: state.books.length),
+          );
+        } else if (state is NewestBooksFailureState) {
+          return SliverToBoxAdapter(
+            child: CustomErrorText(errorMessage: state.errMessage),
+          );
+        } else {
+          return const SliverToBoxAdapter(child: CustomLoadingIndicator());
         }
-        return const BookListItem();
-      }, childCount: length),
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/service_locater.dart';
-import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
-import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
-import 'package:bookly_app/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
