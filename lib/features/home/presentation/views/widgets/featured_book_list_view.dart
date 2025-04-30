86b56f77dedf2db3cbf9ff6692e4f896a925c2a3
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-04-30 14:50:01 +0300

    use cubit in featured list

--- lib/features/home/presentation/views/widgets/featured_book_list_view.dart
+++ lib/features/home/presentation/views/widgets/featured_book_list_view.dart
@@ -1,22 +1,38 @@
+import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_list_view_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedBookListView extends StatelessWidget {
   const FeaturedBookListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 15.0),
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height / 3.625,
-        child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: 15,
-            itemBuilder: (context, index) {
-              return const FeaturedListViewItem();
-            }),
-      ),
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksLoadingState) {
+          return const CircularProgressIndicator();
+        } else if (state is FeaturedBooksSuccessState) {
+          return Padding(
+            padding: const EdgeInsets.only(left: 15.0),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height / 3.625,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: state.books.length,
+                  itemBuilder: (context, index) {
+                    return const FeaturedListViewItem();
+                  }),
+            ),
+          );
+        } else if (state is FeaturedBooksFailureState) {
+          return Container(
+            child: Text(state.errMessage),
+          );
+        } else {
+          return Container(child: Text('Oops there was an error'));
+        }
+      },
     );
   }
 }
