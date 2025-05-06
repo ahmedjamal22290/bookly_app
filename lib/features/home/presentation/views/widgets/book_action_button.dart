7912b233268a785d9c04847fb414e7f0d7211b1d
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-05-06 13:37:25 +0300

    lanch preview link

--- lib/features/home/presentation/views/widgets/book_action_button.dart
+++ lib/features/home/presentation/views/widgets/book_action_button.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class BooksButtonAction extends StatelessWidget {
   const BooksButtonAction({super.key, required this.bookModel});
@@ -39,7 +40,12 @@ class BooksButtonAction extends StatelessWidget {
             ),
           ),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () async {
+              Uri uri = Uri.parse(bookModel.volumeInfo?.previewLink ?? "");
+              if (await canLaunchUrl(uri)) {
+                await launchUrl(uri);
+              }
+            },
             style: const ButtonStyle(
               padding: WidgetStatePropertyAll(EdgeInsets.zero),
             ),
