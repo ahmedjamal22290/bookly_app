030300c0bc6988557420d6183c76019ccf9ceb85
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-05-09 19:03:12 +0300

    add animtion to fav button

--- lib/features/home/presentation/views/widgets/favorite_button.dart
+++ lib/features/home/presentation/views/widgets/favorite_button.dart
@@ -9,28 +9,54 @@ class FavoriteButton extends StatefulWidget {
   State<FavoriteButton> createState() => _FavoriteButtonState();
 }
 
-class _FavoriteButtonState extends State<FavoriteButton> {
+class _FavoriteButtonState extends State<FavoriteButton>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _sizeAnimation;
+
   bool fav = false;
   @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 200));
+
+    _sizeAnimation = Tween<double>(begin: 35, end: 40)
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller.reverse();
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Positioned(
-        top: 200,
-        child: IconButton(
-          onPressed: () {
-            fav = fav ? false : true;
-            setState(() {});
-          },
-          icon: Icon(
-            fav ? Icons.favorite : Icons.favorite_border,
-            size: 35,
-            color: fav ? Colors.red : Colors.grey,
-            shadows: const [
-              Shadow(
-                blurRadius: 30,
-                color: Colors.black,
-              )
-            ],
-          ),
-        ));
+    return AnimatedBuilder(
+      animation: _sizeAnimation,
+      builder: (context, child) {
+        return Positioned(
+            top: 190,
+            child: IconButton(
+              onPressed: () {
+                fav = !fav;
+
+                _controller.forward();
+              },
+              icon: Icon(
+                Icons.favorite,
+                size: _sizeAnimation.value,
+                color: fav ? Colors.red.shade700 : Colors.grey.shade600,
+                shadows: const [
+                  Shadow(
+                    blurRadius: 25,
+                    color: Colors.black,
+                  )
+                ],
+              ),
+            ));
+      },
+    );
   }
 }
